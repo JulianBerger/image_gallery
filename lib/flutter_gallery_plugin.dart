0c341786f5c3664166fbfa623696ffbc9355992f
JulianBerger <jberger95@gmail.com>
2019-08-02 13:49:01 +0200

    Parsing image data

--- lib/flutter_gallery_plugin.dart
+++ lib/flutter_gallery_plugin.dart
@@ -2,10 +2,17 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+class ImageLocation {
+  final num lat;
+  final num lon;
+
+  ImageLocation(this.lat, this.lon);
+}
+
 class ImageData {
   final String path;
-  final location;
-  final time;
+  final ImageLocation location;
+  final num time;
 
   ImageData(this.path, this.location, this.time);
 }
@@ -15,8 +22,16 @@ class FlutterGalleryPlugin {
 
   static const _eventChannel = const EventChannel(DATA_CHANNEL);
 
+  static ImageData _toImageData(dynamic map) {
+  if (map is Map) {
+    ImageLocation location = new ImageLocation(map['location']['lat'], map['location']['lon']);
+    return new ImageData(map['path'], location,  map['time']);
+  }
+  return null;
+  }
+
   static Stream<dynamic> getPhotoData(
   ) {
-    return _eventChannel.receiveBroadcastStream().cast<dynamic>();
+    return _eventChannel.receiveBroadcastStream().map(_toImageData);
   }
 }
